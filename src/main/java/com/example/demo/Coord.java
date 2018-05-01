3c3c4b7d2e4a609b725e435177516f706535a837
Observer189 <aleksandr.kozlovskiy03@mail.ru>
2018-05-01 20:42:49 +0300

    Coord

--- src/main/java/com/example/demo/Coord.java
+++ src/main/java/com/example/demo/Coord.java
@@ -1,11 +1,31 @@
 package com.example.demo;
 
 public class Coord {
-    public Integer x;
-    public Integer y;
+    private Integer x;
+    private Integer y;
+    public Coord()
+        {
+
+    }
     public Coord(Integer x,Integer y)
     {
         this.x=x;
         this.y=y;
+    }
+    public void setX(int x)
+    {
+        this.x=x;
+    }
+    public void setY(int y)
+    {
+        this.y=y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
     }
 }
