a4df359ee39435bba192f2eb31370f490eb89851
Observer189 <aleksandr.kozlovskiy03@mail.ru>
2018-06-02 21:36:59 +0300

    Work please!

--- src/main/java/com/example/demo/ServShip.java
+++ src/main/java/com/example/demo/ServShip.java
@@ -2,35 +2,37 @@ package com.example.demo;
 
 public class ServShip {
     private String name;
-    private ServFixingPoint[] fixingPoints;
+    //private ServFixingPoint[] fixingPoints;
     public ServShip()
     {
-        
+
     }
-    public ServShip(String name,ServFixingPoint[] fixingPoints)
+    public ServShip(String name/*,ServFixingPoint[] fixingPoints*/)
     {
         this.name=name;
-        this.fixingPoints=fixingPoints;
+       // this.fixingPoints=fixingPoints;
     }
     public ServShip(ServShip ship)
     {
        this.name=ship.getName();
-       this.fixingPoints=ship.getFixingPoints();
+       //this.fixingPoints=ship.getFixingPoints();
     }
 
     public String getName() {
         return name;
     }
 
-    public ServFixingPoint[] getFixingPoints() {
-        return fixingPoints;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
 
+    /*public ServFixingPoint[] getFixingPoints() {
+        return fixingPoints;
+    }
+
+    
+
     public void setFixingPoints(ServFixingPoint[] fixingPoints) {
         this.fixingPoints = fixingPoints;
-    }
+    }*/
 }
